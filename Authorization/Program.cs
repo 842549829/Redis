308d5d07d23359938a510efd8bd70d6e552ff10a
liu <liu@SKY-20180811ZXP>
2018-11-09 18:20:43 +0800

    11

---
+++ Authorization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Authorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisClient client = new RedisClient("127.0.0.1", 6379);
+            client.Set<string>("DB1", "CGDFDFDFDF");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                try
+                {
+                    var d = client.Get<string>("DB1");
+                }
+                catch (Exception exxx)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(exxx.Message);
+                }
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
